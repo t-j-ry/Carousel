c5df29896fa2c59552bbd07be840f6d6c06b2f1a
A01252402 <77308137+timjames14@users.noreply.github.com>
2021-04-19 10:46:38 -0700

    javascript v1.0

--- review-carousel.js
+++ review-carousel.js
@@ -1,15 +1,44 @@
 import { data } from "./data.js"
 
-const userJob = () => {
-    data.forEach(user => 
-        user.job
-    )
+class User {
+    constructor(username, imageUrl, job, excerpt) {
+        this.username = username,
+        this.imageUrl = imageUrl,
+        this.job = job,
+        this.excerpt = excerpt
+    }
 }
 
-document.querySelector('#author').innerHTML = data[0].username
+const carouselArr = []
 
-document.querySelector('#person-img').src = data[0].imageUrl
+data.forEach(element => {
+    carouselArr.push(new User(element.username, element.imageUrl, element.job, element.body.excerpt))
+});
 
-document.querySelector('#job').innerHTML = data[0].job
+const displayUser = index => {
+    document.querySelector('#author').innerHTML = carouselArr[index].username,
+    document.querySelector('#person-img').src = carouselArr[index].imageUrl,
+    document.querySelector('#job').innerHTML = carouselArr[index].job,
+    document.querySelector('#info').innerHTML = carouselArr[index].excerpt
+}
 
-document.querySelector('#info').innerHTML = data[0].body.excerpt
+let currIndex = 0
+
+window.onload = displayUser(currIndex)
+
+document.addEventListener('click', function(event){
+    if (event.target.id == 'surprise-Me') {
+        let currIndex = Math.floor(Math.random() * carouselArr.length)
+        displayUser(currIndex)
+    } else if (event.target.classList.contains('bi-arrow-left-circle')) {
+        if (currIndex == 0) {
+            currIndex = carouselArr.length
+        }
+        displayUser(currIndex-=1)
+    } else if (event.target.classList.contains('bi-arrow-right-circle')) {
+        if (currIndex == carouselArr.length-1) {
+            currIndex = -1
+        }
+        displayUser(currIndex+=1)
+    }
+})
